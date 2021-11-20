fbb03c0b0efce92fb549dedf092a6724d8171900
Hayley Song <haejinso@usc.edu>
2021-11-19 17:20:04 -0800

    adds optional class

--- reprlearn/models/plmodules/types_.py
+++ reprlearn/models/plmodules/types_.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Union, Any, TypeVar, Tuple, Dict
+from typing import List, Callable, Union, Optional, Any, TypeVar, Tuple, Dict
 # from torch import tensor as Tensor
 
 Tensor = TypeVar('torch.tensor')
