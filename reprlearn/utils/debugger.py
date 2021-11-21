ea98ba41dbbd1f862b08d4fe36fe618d0d28683b
Hayley Song <haejinso@usc.edu>
2021-11-20 20:18:42 -0800

    adds a helper to check if all params of a nn.Module obj is frozen or there is one that is learnable

--- reprlearn/utils/debugger.py
+++ reprlearn/utils/debugger.py
@@ -1,4 +1,19 @@
 from typing import Any
+import torch.nn as nn
+
+
+def is_frozen(model: nn.Module):
+    for p in model.parameters():
+        if p.requires_grad:
+            return False
+    return True
+
+
+def has_unfrozen_layer(model: nn.Module):
+    for p in model.parameters():
+        if p.requires_grad:
+            return True
+    return False
 
 
 def print_src(pyObj: Any):
