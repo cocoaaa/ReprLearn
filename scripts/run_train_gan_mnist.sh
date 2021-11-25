0af9fe3e9eabaa3ce32083bd8b2df5e5528388f3
Hayley Song <haejinso@usc.edu>
2021-11-24 16:29:41 -0800

    adds a collection of shell cmds to run train_gm.py

---
+++ scripts/run_train_gan_mnist.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+# Usage:
+# Run this script at reprlearn/scripts folder
+# bash run_train_gan_mnist.sh
+
+# Supported datamodules:
+# maptiles, mnist, mnist_m,
+
+# Support plmodules:
+#
+nohup python train_gm.py --model_name conv_fc_gan --data_name mnist \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mnist \
+--gpu_id 1 --max_epochs 500 --batch_size 1028 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--num_generated_sample 10000 \
+--save_sample \
+--save_sample_snapshot \
+--log_sample_to_tb \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M:%s)".txt &
+
+# larger batch_size
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 64 --latent_emb_dim 128 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mnist \
+--gpu_id 1 --max_epochs 500 --batch_size 1028 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+# stronger generator (resnet)
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 64 --latent_emb_dim 128 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mnist \
+--gpu_id 1 --max_epochs 500 --batch_size 1028 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+
+# Train on MNIST-M (MNIST digits on random patch of background)
+#todo: increase max-epochs
+nohup python train_gm.py --model_name conv_fc_gan --data_name mnistm \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mnistm --data_root '/data/hayley-old/Tenanbaum2000/data/MNIST-M' \
+--gpu_id 1 --max_epochs 10 --batch_size 128 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--num_generated_sample 10000 \
+--save_sample \
+--save_sample_snapshot \
+--log_sample_to_tb \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M:%s)".txt &
+
+# Train on Mono_MNIST (MNIST digits on monochrome background)
+#todo: increase max-epochs
+nohup python train_gm.py --model_name conv_fc_gan --data_name mnistm \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mono_mnist --color red --data_root '/data/hayley-old/Tenanbaum2000/data/Mono-MNIST' \
+--gpu_id 1 --max_epochs 10 --batch_size 128 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--num_generated_sample 10000 \
+--save_sample \
+--save_sample_snapshot \
+--log_sample_to_tb \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M:%s)".txt &
+
+# Train on USPS
+#todo: increase max-epochs
+nohup python train_gm.py --model_name conv_fc_gan --data_name mnistm \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name usps --data_root '/data/hayley-old/Tenanbaum2000/data/USPS' \
+--gpu_id 1 --max_epochs 10 --batch_size 128 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--num_generated_sample 10000 \
+--save_sample \
+--save_sample_snapshot \
+--log_sample_to_tb \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M:%s)".txt &
+
+# Train  on usps (n_datapts: 9298; default size: (1,32,32))
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name usps \
+--gpu_id 0 --max_epochs 100 --batch_size 64 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+# todo: use larger batch_size
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name usps \
+--gpu_id 0 --max_epochs 100 --batch_size 128 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 32 --latent_emb_dim 128 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name usps \
+--gpu_id 0 --max_epochs 300 --batch_size 1028 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+# todo: experiment with resnet generator
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 32 --latent_emb_dim 128 \
+--dec_type resnet --dec_hidden_dims 256 128 64 32 \
+--data_name usps \
+--gpu_id 2 --max_epochs 300 --batch_size 1028 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+# todo: Train  on Multi Monochrome MNIST
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name mnistm \
+--gpu_id 0 --max_epochs 3 --batch_size 32 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--n_show 64 \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+
+
+# todo: Train  on Maptile dataset
+nohup python train_gm.py --model_name conv_fc_gan  \
+--latent_dim 10 --latent_emb_dim 32 \
+--dec_type conv --dec_hidden_dims 256 128 64 32 \
+--data_name maptiles \
+--cities la paris \
+--styles CartoVoyagerNoLabels StamenTonerBackground \
+--zooms 14 \
+--gpu_id 0 --max_epochs 100 --batch_size 32 \
+--lr_g 1e-4 --lr_d 1e-3 -k 1 \
+--b1 0.5 --b2 0.9 \
+--ckpt_metric "val/loss_G" \
+--save_top_k 5 \
+--no_early_stop \
+--n_show 64 \
+--log_root /data/hayley-old/Tenanbaum2000/lightning_logs/"$(date +%F)" \
+2>&1 | tee log-"$(date +%F-%H:%M)".txt &
+.
+nohup python train.py --model_name="bivae" \
+--latent_dim=10 --hidden_dims 32 64 128 256 --adv_dim 32 32 32 --adv_weight 15.0 \
+--data_name="maptiles" \
+--cities la paris \
+--styles CartoVoyagerNoLabels StamenTonerBackground \
+--zooms 14 \
+--gpu_id=2 --max_epochs=400   --terminate_on_nan=True  \
+-lr 3e-4 -bs 32 \
+--log_root="/data/hayley-old/Tenanbaum2000/lightning_logs/2021-01-23/" &
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
