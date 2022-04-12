c1b9bfde5a625d7da3bfdd2e0064895927189198
Hayley Song <haejinso@usc.edu>
2022-04-12 16:51:23 -0700

    adds a sampler for kshot(wip?)

--- reprlearn/data/samplers/kshot_sampler.py
+++ reprlearn/data/samplers/kshot_sampler.py
@@ -2,6 +2,7 @@ from reprlearn.data.datasets.base import ImageDataset
 from collections import defaultdict
 from typing import Iterable, Optional, Callable, List, Dict, Tuple
 import numpy as np
+import torch
 
 # ===============
 # Returns a list of datapoints from the dataset so that
@@ -24,12 +25,68 @@ class KShotSampler():
         """
         pass
 
+
+    # note: this is the function being used in the main dataloading step
+    #       (not the `.sample` method)
+    # todo: unify returning format when collate_fn is used and not used
+    def get_support_and_query(
+            self,
+            dset: ImageDataset,
+            num_per_class: int,
+            shuffle: bool=True,
+            collate_fn: Optional[Callable] = None,
+            global_id2local_id: Optional[Dict[int,int]]=None,
+    ) -> Dict:
+
+        def id_map(y):
+            if global_id2local_id is not None:
+                is_tensor = isinstance(y, torch.Tensor)
+                if is_tensor:
+                    y = y.item()
+                y_local = global_id2local_id[y]
+                if is_tensor:
+                    y_local = torch.tensor(y_local)
+                return y_local
+            else:
+                return y
+
+        inds_per_class = self.get_sample_inds_per_class(dset, 2*num_per_class, shuffle)
+        support_inds = []
+        query_inds = []
+        for clabel, cinds in inds_per_class.items():
+            cids = np.fromiter(cinds, dtype=int)
+            support_inds.append(cids[:num_per_class])
+            query_inds.append(cinds[num_per_class:])
+
+        support_inds = np.array(support_inds).flatten()
+        query_inds = np.array(query_inds).flatten()
+
+        # we don't want to load imgs for one-class all in a row,
+        # and then next class's images in a row, etc
+        np.random.shuffle(support_inds)
+        support_sample = [dset[i] for i in support_inds]  # apply current dataset's image transform if specified
+        if collate_fn is not None:
+            batch_x = collate_fn([dpt[0] for dpt in support_sample])
+            batch_y = collate_fn([id_map(dpt[1]) for dpt in support_sample])
+            support_sample = (batch_x, batch_y)
+
+        # Similarly for the query sample
+        np.random.shuffle(query_inds)
+        query_sample = [dset[i] for i in query_inds]  # apply current dataset's image transform if specified
+        if collate_fn is not None:
+            batch_x = collate_fn([dpt[0] for dpt in query_sample])
+            batch_y = collate_fn([id_map(dpt[1]) for dpt in query_sample])
+            query_sample = (batch_x, batch_y)
+        return {'support': support_sample,
+                'query': query_sample}
+
+
     def get_sample_inds_per_class(self,
                                   dset: ImageDataset,
                                   num_per_class: int,
                                   shuffle: bool=True,
                                   verify: bool=True,
-                                ):
+                                ) -> Dict[int,List]:
         """Given the dataset of labelled images, return the indices for sampling
        `num_per_class` number of images per class in the dataset's classes.
        If shuffle, we shuffle the indices of the dset for each call to the iterator
@@ -50,6 +107,8 @@ class KShotSampler():
         done_for_class = {c:False for c in unique_classes}
         for i in inds:
             c = dset.targets[i]
+            if isinstance(c, torch.Tensor):
+                c = c.item()
             if not done_for_class[c]:  # len(inds_per_class[c]) < num_per_class:
                 inds_per_class[c].append(i)
                 if len(inds_per_class[c]) == num_per_class:
@@ -58,7 +117,7 @@ class KShotSampler():
             if np.alltrue(np.fromiter(done_for_class.values(), dtype=bool)):
                 break
 
-        print("Done collecting datapts for each class...")
+        # print("Done collecting datapts for each class...")
         if verify:
             for c in np.unique(dset.targets):
                 inds = inds_per_class[c]
@@ -66,12 +125,14 @@ class KShotSampler():
                     raise ValueError
         return inds_per_class
 
+
+    # todo: deprecate?
     def sample(self,
-        dset: ImageDataset,
-        num_per_class: int,
-        shuffle: bool=True,
-        collate_fn: Optional[Callable]=None
-    ) -> List[Tuple]: # [(x,y),...]  #List[int]:
+               dset: ImageDataset,
+               num_per_class: int,
+               shuffle: bool = True,
+               collate_fn: Optional[Callable] = None
+               ) -> List[Tuple]:  # [(x,y),...]  #List[int]:
         """Given the dataset of labelled images, return the collection/list
         of datapoints from the dataset; the collection of datapoints (aka. sample)
         contains equal number of datapoints per class (with best effort)
@@ -102,43 +163,8 @@ class KShotSampler():
         # and then next class's images in a row, etc
         np.random.shuffle(sample_inds)
 
-        sample = [dset[i] for i in sample_inds] # apply current dataset's image transform if specified
+        sample = [dset[i] for i in sample_inds]  # apply current dataset's image transform if specified
         if collate_fn is not None:
             sample = collate_fn(sample)
         return sample
-
-    def get_support_and_query(
-            self,
-            dset: ImageDataset,
-            num_per_class: int,
-            shuffle: bool=True,
-            collate_fn: Optional[Callable] = None
-    ) -> Dict:
-        inds_per_class = self.get_sample_inds_per_class(dset, 2*num_per_class, shuffle)
-        n_way = len(np.unique(dset.targets))
-        support_inds = []
-        query_inds = []
-        for clabel, cinds in inds_per_class.items():
-            cids = np.fromiter(cinds, dtype=int)
-            support_inds.append(cids[:num_per_class])
-            query_inds.append(cinds[num_per_class:])
-
-        support_inds = np.array(support_inds)
-        query_inds = np.array(query_inds)
-
-        # we don't want to load imgs for one-class all in a row,
-        # and then next class's images in a row, etc
-        np.random.shuffle(support_inds)
-        support_sample = [dset[i] for i in support_inds]  # apply current dataset's image transform if specified
-        if collate_fn is not None:
-            support_sample = collate_fn(support_sample)
-
-        # Similarly for the query sample
-        np.random.shuffle(query_inds)
-        query_sample = [dset[i] for i in query_inds]  # apply current dataset's image transform if specified
-        if collate_fn is not None:
-            query_sample = collate_fn(query_sample)
-        return {'support': support_sample,
-                'query': query_sample}
-
 
